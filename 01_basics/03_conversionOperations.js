f1aecee2e81a9bc33e0f9ad9df2f747f9efaae79
nasir1976 <148912534+nasir1976@users.noreply.github.com>
2024-03-21 22:04:56 +0000

    update

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "35"
+
+let scoretwo = "35ABC"
+
+let scorethree = null
+
+
+console.log(typeof score)
+console.log(typeof (score))
+console.log(scorethree)
+
+let valueInNumber = Number(score)
+let valueInNumberTwo = Number(scoretwo)
+let valueInNumberThree = Number(scorethree)
+
+console.log(valueInNumber)
+console.log(valueInNumberTwo)
+console.log(valueInNumberThree)
+
+// "35" => 35
+// "35abc" => NaN
+// true => 1; false => 0
+
+
+let isLogededIn = 
