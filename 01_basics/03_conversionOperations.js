cab48e2498160dbf5ddccd1d3c59384fb2fc3e64
nasir1976 <148912534+nasir1976@users.noreply.github.com>
2024-03-22 05:26:57 +0000

    done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -4,22 +4,45 @@ let scoretwo = "35ABC"
 
 let scorethree = null
 
+let scorefour = undefined
+
+
 
 console.log(typeof score)
 console.log(typeof (score))
 console.log(scorethree)
+console.log(scorefour)
 
 let valueInNumber = Number(score)
 let valueInNumberTwo = Number(scoretwo)
 let valueInNumberThree = Number(scorethree)
+let valueInNumberFour = Number(scorefour)
 
 console.log(valueInNumber)
 console.log(valueInNumberTwo)
 console.log(valueInNumberThree)
+console.log(valueInNumberFour)
 
 // "35" => 35
 // "35abc" => NaN
 // true => 1; false => 0
 
 
-let isLogededIn = 
+// let isLogededIn = 1
+// let isLogededIn = ""
+let isLogededIn = "ahmed"
+
+let booleanIsLogedIn = Boolean (isLogededIn)
+
+console.log(booleanIsLogedIn)
+
+// 1 => true; 0 => false
+// "" => false; 
+// "ahmed" => true
+
+
+let someNumber = 35
+let stringSomeNumber = String(someNumber)
+
+console.log(stringSomeNumber)
+console.log(typeof stringSomeNumber)
