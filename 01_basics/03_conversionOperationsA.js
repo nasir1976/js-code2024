4096d8e8c0fe0d42811d1eee4dc57f89ba29956e
nasir1976 <148912534+nasir1976@users.noreply.github.com>
2024-03-22 08:39:33 +0000

    update

--- 01_basics/03_conversionOperationsA.js
+++ 01_basics/03_conversionOperationsA.js
@@ -28,7 +28,39 @@ console.log("1" + (2 + 2)) // result 14
 console.log(1 + 2 + "2") // result 32
 console.log((1 + 2) + "2") // result 32
 
+console.log((3 + 4) * 5 % 3)
+
+console.log(true) // result true
+console.log(+true) // result 1 -- don't use
+console.log(+"") // result 0 -- don't use
+// console.log(true+) // error result 1
+
+// -- don't use
+let num1, num2, num3
+num1 = num2 = num3 = 2+2 // result 4
+console.log(num1)
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+let gameCounterTwo = 100
+console.log(++gameCounterTwo)
 
 
 
 
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+// link to study https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment#postfix_increment
