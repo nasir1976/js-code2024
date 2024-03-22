0b91515c3979484dabb2f1bb66f23f72ea8db92a
nasir1976 <148912534+nasir1976@users.noreply.github.com>
2024-03-22 21:56:41 +0000

    straing update

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,9 @@
+const name = "ahmed"
+const repoCount = 50
+
+console.log(name + repoCount + " Value");
+console.log(`His name ${name} age ${repoCount}`);
+
+const gameName = new String("ahmed");
+
+console.log(gameName)
