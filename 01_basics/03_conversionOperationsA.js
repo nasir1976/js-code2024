17504dfad8e0714a8d33658592292336b9be5be1
nasir1976 <148912534+nasir1976@users.noreply.github.com>
2024-03-22 05:47:47 +0000

    done

---
+++ 01_basics/03_conversionOperationsA.js
@@ -0,0 +1,34 @@
+// ***** Operations ***** //
+
+let value = 3
+let negValue = -value
+
+console.log(typeof negValue)
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(3-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+
+let str1 = "Hello"
+let str2 = " Ahmed"
+
+console.log(str1 + str2)
+
+console.log(1 + "2") // result 12
+console.log("1" + 2) // result 12
+
+console.log("1" + 2 + 2) // result 112
+console.log("1" + (2 + 2)) // result 14
+
+console.log(1 + 2 + "2") // result 32
+console.log((1 + 2) + "2") // result 32
+
+
+
+
+
