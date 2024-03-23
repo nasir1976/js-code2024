2dc145ad917f1b7f36ac57a259af0fec44d28cf7
nasir1976 <148912534+nasir1976@users.noreply.github.com>
2024-03-23 07:23:22 +0000

    string done

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,9 +1,38 @@
-const name = "ahmed"
+const name = "ahmed-khan"
 const repoCount = 50
 
 console.log(name + repoCount + " Value");
 console.log(`His name ${name} age ${repoCount}`);
 
-const gameName = new String("ahmed");
+const gameName = new String("ahmed-khan");
 
-console.log(gameName)
+console.log(gameName[0]);
+
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase())
+
+console.log(gameName.charAt(2))
+
+console.log(gameName.indexOf('h'))
+
+const newString = gameName.substring(0, 3);
+
+const newStringA = gameName.substring(1, 3);
+
+const anOtherString = gameName.slice(-6, 4);
+
+console.log(newString)
+console.log(newStringA)
+console.log(anOtherString)
+
+const newStringOne = "   Ahmed "
+const newStringTwo = "  Ahmed-Khan-"
+
+console.log(newStringOne)
+console.log(newStringOne.trim())
+console.log(newStringTwo.replace('-', '_').trim())
+
+console.log(gameName.split('-'))
