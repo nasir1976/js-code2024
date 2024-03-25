59fcc87aa8381fd064d62a1f507bbcf8d428bc1f
nasir1976 <148912534+nasir1976@users.noreply.github.com>
2024-03-25 09:51:05 +0000

    nums and math

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,36 @@
+const score = 400
+
+const balance = new Number(500)
+
+const otherBalance = new Number(232.5543)
+const otherBalanceTwo = 384522205
+const otherBalanceThree = 384522205
+const otherBalanceFour = 384522205
+const otherBalanceFive = 384522205
+
+
+
+
+
+console.log(score)
+console.log(balance)
+
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(2))
+
+console.log(otherBalance.toPrecision(5))
+
+console.log(otherBalance.toPrecision(4))
+
+console.log(otherBalanceTwo.toLocaleString())
+console.log(otherBalanceThree.toLocaleString('en-IN'))
+console.log(otherBalanceFour.toLocaleString('fr-FR'))
+console.log(otherBalanceFive.toLocaleString('en-PK'))
+
+
+
+
+
+
+
