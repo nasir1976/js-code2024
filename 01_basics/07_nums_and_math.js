dfefb058d7a38fd03caeeefc9be96ea51cb19242
nasir1976 <148912534+nasir1976@users.noreply.github.com>
2024-03-25 10:24:55 +0000

    Number and Maths

--- 01_basics/07_nums_and_math.js
+++ 01_basics/07_nums_and_math.js
@@ -8,10 +8,6 @@ const otherBalanceThree = 384522205
 const otherBalanceFour = 384522205
 const otherBalanceFive = 384522205
 
-
-
-
-
 console.log(score)
 console.log(balance)
 
@@ -27,6 +23,37 @@ console.log(otherBalanceTwo.toLocaleString())
 console.log(otherBalanceThree.toLocaleString('en-IN'))
 console.log(otherBalanceFour.toLocaleString('fr-FR'))
 console.log(otherBalanceFive.toLocaleString('en-PK'))
+
+
+// +++++++++++++++++++++ Math ++++++++++++++++++++++++
+
+console.log(Math.abs(-4))
+
+console.log(Math.round(4.65))
+console.log(Math.ceil(4.65))
+console.log(Math.floor(4.65))
+
+console.log(Math.sqrt(25))
+
+console.log(Math.max(2,4,1,6,8))
+console.log(Math.min(2,4,1,6,8))
+
+console.log(Math.random())
+
+console.log(Math.random() * 10 + 1)
+console.log((Math.random() * 10) + 1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+
+
+console.log((Math.random() * 10) + 11)
+console.log(Math.floor(Math.random() * 10) + 11)
+
+
+
+
 
 
 
