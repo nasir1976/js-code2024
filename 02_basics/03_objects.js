2b4c36ac666abdef68f5bcb302f78135248e3c74
nasir1976 <148912534+nasir1976@users.noreply.github.com>
2024-03-28 10:11:41 +0000

    object update

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,9 +1,6 @@
-// singleton
+// Creating and Accessing Object Properties
 
-
-// objects literals
-
-const mySym = Symbol("key1")
+const mySym = Symbol("key1");
 
 const jsUser = {
     name: "Ahmed",
@@ -14,31 +11,43 @@ const jsUser = {
     location: "Lahore",
     isLoggedIn: false,
     lastLoginDays: ["Sunday", "Monday", "Friday",]
-}
-console.log(jsUser.age)
-console.log(jsUser["name"])
-console.log(jsUser["full name"])
-console.log(jsUser[mySym])
+};
 
-jsUser.email = "ahmed@hotmail.com"
-
-console.log(jsUser["email"])
-
-// Object.freeze(jsUser)
-
-jsUser.email = "ahmed@gmail.com"
-
-console.log(jsUser)
+// Accessing object properties
+console.log(jsUser.age); // 18
+console.log(jsUser["name"]); // "Ahmed"
+console.log(jsUser["full name"]); // "Ahmed Khan"
+console.log(jsUser[mySym]); // "mykey1"
 
 
-jsUser.greeting = function(){
-    console.log("Hello JS user ")
-}
 
-jsUser.greetingTwo = function(){
-    console.log(`Hello JS user ${this.name}`)
-}
+// Modifying Object Properties
+// Modifying an object property
+jsUser.email = "ahmed@hotmail.com";
+console.log(jsUser["email"]); // "ahmed@hotmail.com"
 
-console.log(jsUser.greeting)
+// Attempt to change email again
+jsUser.email = "ahmed@gmail.com";
+console.log(jsUser.email); // "ahmed@gmail.com"
 
-console.log(jsUser.greetingTwo())
+
+// Object Methods
+// Adding methods to the object
+jsUser.greeting = function() {
+    console.log("Hello JS user");
+};
+
+jsUser.greetingTwo = function() {
+    console.log(`Hello JS user ${this.name}`);
+};
+
+// Since jsUser.greeting is a function, logging it will show the function definition
+console.log(jsUser.greeting);
+
+// Calling the method
+jsUser.greetingTwo(); // "Hello JS user Ahmed"
+
+
+
+
+
