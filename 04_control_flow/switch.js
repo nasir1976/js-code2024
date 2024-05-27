484723cd41455ab5d25b6a478e9b209e16c423fb
nasir1976 <148912534+nasir1976@users.noreply.github.com>
2024-05-27 07:48:52 +0000

    save

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,8 +1,45 @@
-switch (key) {
-    case value:
-        
-        break;
+const month = 3
 
+switch (month) {
+    case 1:
+        console.log("Jan")   
+        break;
+    case 2:
+        console.log("Feb")   
+        break;
+    case 3:
+        console.log("Mar")  
+        break;
+    case 4:
+        console.log("Apr")   
+        break;
+    default: "Default case match"
+        break;
+}
+
+
+const day = "tuesday"
+
+switch (day) {
+    case "monday":
+        console.log("Mon")   
+        break;
+    case "tuesday":
+        console.log("Tue")   
+        break;
+    case "thursday":
+        console.log("Thu")   
+        break;
+    case "friday":
+        console.log("Fri")   
+        break;
+    case "saturday":
+        console.log("Sat")   
+        break;
+    case "sunday":
+        console.log("Sun")   
+        break;
     default:
+        console.log("Default case match")
         break;
 }
